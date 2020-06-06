e16295dedb45bd4b6da2d1cb46a9376c2089f7b1
venkatesh405 <58566543+venkatesh405@users.noreply.github.com>
2020-06-06 13:38:38 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Libs.kotlinVersion}")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Libs.navigationVersion}")
+        classpath("com.google.gms:google-services:4.3.3")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.1.1")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register("clean", Delete::class.java) {
+    delete(rootProject.buildDir)
+}
